4e588e4c6084501b6909d389575bfe1de04cc4cf
oana-iu <oana.mitri01@gmail.com>
2023-12-02 23:34:12 +0200

    Add smooth scrolling

--- js/main.js
+++ js/main.js
@@ -10,3 +10,35 @@ async function initMap() {
 }
 
 initMap();
+
+//Smooth scrolling
+
+document.addEventListener("DOMContentLoaded", scroll);
+
+function scroll() {
+    const navLinks = document.querySelectorAll("nav a");
+    const btn = document.querySelector(".btn")
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", smoothScroll);
+    });
+    btn.addEventListener("click", smoothScroll)
+}
+
+function smoothScroll(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+}
+
+window.addEventListener('scroll', function(){
+    if(this.window.scrollY > 150) {
+     document.querySelector("#navbar").style.opacity = 0.9;
+    } else {
+        document.querySelector("#navbar").style.opacity = 0.9;
+    }
+})
